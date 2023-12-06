c559fab64c541d8b298c5d9fa3d94b9fe517ca7a
Jose Angulo <jjavier1708@gmail.com>
2023-12-05 21:14:40 -0700

    Add API custom routes for the prompt

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,8 +7,8 @@ import { useRouter } from 'next/navigation';
 import Profile from '@components/profile';
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
-
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -19,12 +19,32 @@ const MyProfile = () => {
       setPosts(data);
     };
 
-    if(session?.user.id) fetchPosts();
+    if (session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {};
+  const handleEdit = post => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-  const handleDelete = async () => {};
+  const handleDelete = async post => {
+    const hasConfirmed = confirm(
+      'Are you sure you want to delete this prompt?'
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPosts = posts.filter(p => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   return (
     <Profile
