7d89af4ac70d40494494fea3e205859ee1f0f466
Jose Angulo <jjavier1708@gmail.com>
2023-12-10 17:35:11 -0700

    Implement filter functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,24 +5,24 @@ import { useState, useEffect } from 'react';
 import PromptCard from './PromptCard';
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  return(
+  return (
     <div className='mt-16 prompt_layout'>
-      {data.map((post) => (
-        <PromptCard 
+      {data.map(post => (
+        <PromptCard
           key={post._id}
           post={post}
           handleTagClick={handleTagClick}
         />
       ))}
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedPosts, setSearchedPosts] = useState([]);
   const [posts, setPosts] = useState([]);
-
-  const handleSearchChange = e => {};
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -30,15 +30,49 @@ const Feed = () => {
       const data = await response.json();
 
       setPosts(data);
-    }
+    };
 
     fetchPosts();
-  }, [])
-  
+  }, []);
+
+  const filterPrompts = searchText => {
+    const regex = new RegExp(searchText, 'i');
+    return posts.filter(
+      item =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = e => {
+    e.preventDefault();
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedPosts(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = tagName => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedPosts(searchResult);
+  };
 
   return (
     <section className='feed'>
-      <form className='relative w-full flex-center'>
+      <form
+        className='relative w-full flex-center'
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
         <input
           type='text'
           placeholder='Search for a tag or a username'
@@ -49,9 +83,9 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList 
-        data={posts}
-        handleTagClick={() => {}}
+      <PromptCardList
+        data={searchText !== '' ? searchedPosts : posts}
+        handleTagClick={handleTagClick}
       />
     </section>
   );
